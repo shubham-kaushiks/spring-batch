97e52ac7f154ab1a1b638a72e1ef116d5974ed6e
Shubham Kaushik <shubhamkaushik.jiit@gmail.com>
2020-05-05 14:41:36 +0530

    Tried autowiring of tasklets

--- src/main/java/com/example/starter/StarterApplication.java
+++ src/main/java/com/example/starter/StarterApplication.java
@@ -1,36 +1,16 @@
 package com.example.starter;
 
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableBatchProcessing
 public class StarterApplication{
-
-	@Autowired
-    JobLauncher jobLauncher;
-     
-    @Autowired
-    Job job;
-    
 	public static void main(String[] args) {
 		SpringApplication.run(StarterApplication.class, args);
 	}
-	
-    @Scheduled(cron = "0 */1 * * * ?")
-    public void perform() throws Exception 
-    {
-        JobParameters params = new JobParametersBuilder()
-                .addString("JobID", String.valueOf(System.currentTimeMillis()))
-                .toJobParameters();
-        jobLauncher.run(job, params);
-    }
 	
 }
--- src/main/java/com/example/starter/config/BatchConfig.java
+++ src/main/java/com/example/starter/config/BatchConfig.java
@@ -4,21 +4,26 @@ import javax.sql.DataSource;
 
 import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.BatchConfigurer;
 import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import com.example.starter.tasks.MyTaskOne;
 import com.example.starter.tasks.MyTaskTwo;
+
 
 @Configuration
 @EnableBatchProcessing
@@ -29,27 +34,50 @@ public class BatchConfig {
  
     @Autowired
     private StepBuilderFactory steps;
+    
+    @Autowired
+    JobLauncher jobLauncher;
+    
+//    @Autowired
+//    private Job job1;
+//
+//    @Autowired
+//    private Job job2;
      
+    @Autowired
+    private MyTaskOne m;
+    
+    @Autowired
+    private MyTaskTwo n;
+    
     @Bean
     public Step stepOne(){
         return steps.get("stepOne")
-                .tasklet(new MyTaskOne())
+                .tasklet(m)
                 .build();
     }
      
     @Bean
     public Step stepTwo(){
         return steps.get("stepTwo")
-                .tasklet(new MyTaskTwo())
+                .tasklet(n)
                 .build();
     }   
      
     @Bean
-    public Job demoJob(){
-        return jobs.get("demoJob")
+    public Job job1(){
+        return jobs.get("job1")
                 .incrementer(new RunIdIncrementer())
                 .start(stepOne())
                 .next(stepTwo())
+                .build();
+    }
+    
+    @Bean
+    public Job job2(){
+        return jobs.get("job2")
+                .incrementer(new RunIdIncrementer())
+                .start(stepTwo())
                 .build();
     }
     
@@ -77,5 +105,31 @@ public class BatchConfig {
     		    return factory.getObject();
     		}
     	};
+    } 
+    
+    @Scheduled(cron = "0 */1 * * * ?")
+    public void run1(){
+    	JobParameters params = new JobParametersBuilder()
+                .addString("JobID", String.valueOf(System.currentTimeMillis()))
+                .toJobParameters();
+    	try {
+            jobLauncher.run(job1(), params);
+        }catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+
+    }
+    
+    @Scheduled(cron = "0 */2 * * * ?")
+    public void run2() {
+    	JobParameters params = new JobParametersBuilder()
+                .addString("JobID", String.valueOf(System.currentTimeMillis()))
+                .toJobParameters();
+    	try {
+            jobLauncher.run(job2(), params);
+        }catch (Exception ex){
+            System.out.println(ex.getMessage());
+        }
+    	
     }
 }
